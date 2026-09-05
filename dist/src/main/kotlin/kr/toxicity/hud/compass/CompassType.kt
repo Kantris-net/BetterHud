5cf5b89ac91a9adc0c2d5ea1fc3b0001e736f60a
Luna <60011425+akshualy@users.noreply.github.com>
2026-09-06 00:03:42 +0200

    port previous reverse compass impl

--- dist/src/main/kotlin/kr/toxicity/hud/compass/CompassType.kt
+++ dist/src/main/kotlin/kr/toxicity/hud/compass/CompassType.kt
@@ -2,6 +2,7 @@ package kr.toxicity.hud.compass
 
 import kr.toxicity.hud.api.yaml.YamlObject
 import kr.toxicity.hud.compass.type.CircleCompass
+import kr.toxicity.hud.compass.type.ReverseCircleCompass
 import kr.toxicity.hud.resource.GlobalResource
 import java.io.File
 
@@ -14,6 +15,16 @@ enum class CompassType {
             section: YamlObject
         ): CompassImpl {
             return CircleCompass(resource, assets, name, section)
+        }
+    },
+    REVERSE_CIRCLE {
+        override fun build(
+            resource: GlobalResource,
+            assets: File,
+            name: String,
+            section: YamlObject
+        ): CompassImpl {
+            return ReverseCircleCompass(resource, assets, name, section)
         }
     }
     ;
